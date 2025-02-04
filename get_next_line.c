6092f30a42de362bd3d911e7e3d90a197b5cacba
dacarras <danielcarrascotocagmail.com>
2025-02-04 20:03:37 +0100

    Gnl1

---
+++ get_next_line.c
@@ -0,0 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dacarras <dacarras@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 16:53:52 by dacarras          #+#    #+#             */
+/*   Updated: 2025/02/04 19:51:36 by dacarras         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char *read_line(int fd, char *storage)
+{
+	char	*buff;
+	int		dim;
+
+	/* quizas haya que usar ft_calloc */
+	buff = ft_calloc((sizeof(char)), (BUFFER_SIZE + 1));
+	if (!buff)
+	{
+		free(buff);
+		return (NULL);
+	}
+	dim = 1;
+	while (!(ft_strchr(storage, '\n')) && dim != 0)
+	{
+		dim = read(fd, buff, BUFFER_SIZE);
+		if (dim < 0)
+		{
+			free(buff);
+			free(storage);
+			return (NULL);
+		}
+		buff[dim] = '\0';
+		storage = ft_strjoin(storage, buff);
+	}
+	free(buff);
+	return (storage);
+}
+
+static char *save_extra(char *storage)
+{
+	int i;
+	int j;
+	char *buff;
+
+	i = 0;
+	while (storage[i] != '\n' && storage[i])
+		i++;
+	if(!storage[i])
+	{
+		free(storage);
+		return (NULL);
+	}
+	buff = malloc(sizeof(char) * (ft_strlen(storage) - i + 1));
+	if (!buff)
+	{
+		free(buff);
+		return (NULL);
+	}
+	j = 0;
+	i++;
+	while (storage[i])
+		buff[j++] = storage[i++];
+	buff[j] = '\0';
+	free(storage);
+	return (buff);
+}
+
+static char *save_line(char *storage)
+{
+	char *buff;
+	int i;
+	int j;
+
+	if (storage[0] == 0)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (storage[i] != '\n' && storage[i])
+		i++;
+	buff = ft_calloc((sizeof(char *)), (i + 1));
+	if (!buff)
+		return (NULL);
+	while (j <= i)
+	{
+		buff[j] = storage[j];
+		j++;
+	}
+	buff[j] = '\0';//j+1again
+	return (buff);
+}
+
+char *get_next_line(int fd)
+{
+	char *line;
+	static char *storage;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	storage = read_line(fd, storage);
+	if (!storage)
+		return (NULL);
+	line = save_line(storage);
+	storage = save_extra(storage);
+	return (line);
+}
+
+/* int main()
+{
+	int fd = open("empty.txt", O_RDONLY);
+	char *r;
+	r = get_next_line(fd);
+	while (r)
+	{
+		free(r);
+		r = get_next_line(fd);
+		printf("%s", r);
+
+	}
+	close(fd);
+	return 0;
+}  */
---
+++ get_next_line_utils.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dacarras <dacarras@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 16:53:49 by dacarras          #+#    #+#             */
+/*   Updated: 2025/02/04 19:31:25 by dacarras         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char		*endstr;
+	size_t		totlen;
+	int			i;
+	int			j;
+
+	totlen = ft_strlen(s1) + ft_strlen(s2);
+	endstr = ft_calloc(sizeof(char), (totlen + 1));
+	if (!endstr)
+	{
+		free(endstr);
+		return (NULL);
+	}
+	i = 0;
+	while (s1 && s1[i])
+	{
+		endstr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2 && s2[j])
+		endstr[i++] = s2[j++];
+	free(s1);
+	return (endstr);
+}
+
+size_t	ft_strlen(char *s)
+{
+	size_t	size;
+
+	size = 0;
+	while (s && s[size])
+		size++;
+	return (size);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	if (!s)
+		return (0);
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == *s)
+		return ((char *)s);
+	return (NULL);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	char		*p;
+	size_t		i;
+	size_t		n;
+
+	n = size * nmemb;
+	p = (char *) malloc(n);
+	if (!p)
+
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		p[i] = 0;
+		i++;
+	}
+	return (p);
+}
